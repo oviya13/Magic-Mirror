258ba87250594de043d28973c87547f07b90f332
oviya13 <96100573+oviya13@users.noreply.github.com>
2023-03-09 11:34:23 +0530

    Create Modules.js

---
+++ Modules.js
@@ -0,0 +1,10 @@
+modules: [
+  {
+    module: "helloworld",
+    position: "bottom_bar", // This can be any of the regions.
+    config: {
+      // See 'Configuration options' for more information.
+      text: "Hello world!",
+    },
+  },
+];
